dd2a03ba07fcc2bcdaac513e1244288f5fbea086
Joymg <joy.marquezg@gmail.com>
2025-02-28 15:10:30 +0100

    Add GroundDetector to namespace

--- Assets/Scripts/GroundDetector.cs
+++ Assets/Scripts/GroundDetector.cs
@@ -3,57 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundDetector : MonoBehaviour
+namespace Joymg.Platformer2D.Detectors
 {
-    public Collider2D agentCollider;
-    public LayerMask groundMask;
-
-    public bool isGrounded = false;
-
-    [Header("Gizmo parameters")] [Range(-2f, 2f)] [SerializeField]
-    private float boxCastYOffset = -0.1f;
-
-    [Range(-2f, 2f)] [SerializeField] private float boxCastXOffset = -0.1f;
-
-    [Range(0f, 2f)] [SerializeField] private float boxCastWidth = 1, boxCastHeight = 1;
-
-    [SerializeField] private Color groundedGizmosColor = Color.green, airborneGizmoColor = Color.red;
-
-    private Vector2 BoxCastCenter => agentCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset);
-    private Vector2 BoxCastSize => new(boxCastWidth, boxCastHeight);
-
-    private void Awake()
+    public class GroundDetector : MonoBehaviour
     {
-        if (!agentCollider)
-            agentCollider = GetComponent<Collider2D>();
-    }
+        public Collider2D agentCollider;
+        public LayerMask groundMask;
 
-    public void CheckIsGrounded()
-    {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(BoxCastCenter, BoxCastSize, 0, Vector2.down, 0, groundMask);
+        public bool isGrounded = false;
 
-        if (raycastHit.collider)
+        [Header("Gizmo parameters")] [Range(-2f, 2f)] [SerializeField]
+        private float boxCastYOffset = -0.1f;
+
+        [Range(-2f, 2f)] [SerializeField] private float boxCastXOffset = -0.1f;
+
+        [Range(0f, 2f)] [SerializeField] private float boxCastWidth = 1, boxCastHeight = 1;
+
+        [SerializeField] private Color groundedGizmosColor = Color.green, airborneGizmoColor = Color.red;
+
+        private Vector2 BoxCastCenter => agentCollider.bounds.center + new Vector3(boxCastXOffset, boxCastYOffset);
+        private Vector2 BoxCastSize => new(boxCastWidth, boxCastHeight);
+
+        private void Awake()
         {
-            if (raycastHit.collider.IsTouching(agentCollider))
+            if (!agentCollider)
+                agentCollider = GetComponent<Collider2D>();
+        }
+
+        public void CheckIsGrounded()
+        {
+            RaycastHit2D raycastHit = Physics2D.BoxCast(BoxCastCenter, BoxCastSize, 0, Vector2.down, 0, groundMask);
+
+            if (raycastHit.collider)
             {
-                isGrounded = true;
+                if (raycastHit.collider.IsTouching(agentCollider))
+                {
+                    isGrounded = true;
+                }
+            }
+            else
+            {
+                isGrounded = false;
             }
         }
-        else
+
+        private void OnDrawGizmos()
         {
-            isGrounded = false;
+            if (!agentCollider)
+                return;
+
+            Gizmos.color = airborneGizmoColor;
+            if (isGrounded)
+                Gizmos.color = groundedGizmosColor;
+
+            Gizmos.DrawWireCube(BoxCastCenter, BoxCastSize);
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (!agentCollider)
-            return;
-
-        Gizmos.color = airborneGizmoColor;
-        if (isGrounded)
-            Gizmos.color = groundedGizmosColor;
-
-        Gizmos.DrawWireCube(BoxCastCenter, BoxCastSize);
     }
 }
