af26943f95194be78c76a9a2c3b213da4475c87f
Joymg <joy.marquezg@gmail.com>
2025-03-12 16:16:46 +0100

    Create Hit and Dead State

---
+++ Assets/Scripts/StatePattern/DeadState.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using Joymg.Platformer2D.Entities;
+using UnityEngine;
+
+namespace Joymg.Platformer2D.States
+{
+    public class DeadState : AgentState
+    {
+        [SerializeField] private float timeToWaitBeforeRespawn = 1f;
+
+        protected override void PerformEnter()
+        {
+            base.PerformEnter();
+            _agent.animatorManager.PlayAnimation(AnimationType.Dead);
+            _agent.animatorManager.OnAnimationEnd.AddListener(WaitBeforeDie);
+            _agent.body.isKinematic = true;
+            _agent.agentInput.enabled = false;
+        }
+
+        protected override void HandleJumpPressed()
+        {
+            //prevent jump
+        }
+
+        protected override void HandleAttack()
+        {
+            //prevent attack
+        }
+
+        public override void UpdateState()
+        {
+            _agent.body.velocity = new Vector2(0, _agent.body.velocity.y);
+        }
+
+        public override void FixedUpdateState()
+        {
+            //
+        }
+
+        public override void Die()
+        {
+            //prevent dead again while dying
+        }
+
+        private void WaitBeforeDie()
+        {
+            _agent.animatorManager.OnAnimationEnd.RemoveListener(WaitBeforeDie);
+            StartCoroutine(WaitCoroutine());
+        }
+
+        private IEnumerator WaitCoroutine()
+        {
+            yield return new WaitForSeconds(timeToWaitBeforeRespawn);
+            _agent.OnDead?.Invoke();
+        }
+
+        public override void ExitState()
+        {
+            base.ExitState();
+            StopAllCoroutines();
+            _agent.animatorManager.ResetEvents();
+            _agent.body.isKinematic = false;
+            _agent.agentInput.enabled = true                  ;
+        }
+    }
+}
---
+++ Assets/Scripts/StatePattern/HitState.cs
@@ -0,0 +1,43 @@
+﻿using Joymg.Platformer2D.Entities;
+
+namespace Joymg.Platformer2D.States
+{
+    public class HitState : AgentState
+    {
+        protected override void PerformEnter()
+        {
+            base.PerformEnter();
+            _agent.animatorManager.PlayAnimation(AnimationType.Hit);
+            _agent.animatorManager.OnAnimationEnd.AddListener(Recover);
+        }
+
+        protected override void HandleAttack()
+        {
+            //prevent attack
+        }
+
+        protected override void HandleJumpPressed()
+        {
+            //prevent jump
+        }
+
+        public override void GetHit()
+        {
+            //prevent getting hit
+        }
+
+        public override void UpdateState()
+        {
+        }
+
+        public override void FixedUpdateState()
+        {
+        }
+
+        private void Recover()
+        {
+            _agent.animatorManager.OnAnimationEnd.RemoveListener(Recover);
+            _agent.SetState(_agent.StateFactory.GetState(StateType.Idle));
+        }
+    }
+}
