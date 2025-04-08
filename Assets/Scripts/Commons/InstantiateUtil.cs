96d08bb752cf124503caf745ceaa6686896f007e
Joymg <joy.marquezg@gmail.com>
2025-04-08 18:51:04 +0200

    Enemy drop loot script

---
+++ Assets/Scripts/Commons/InstantiateUtil.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Joymg.Platformer2D.Commons
+{
+    public class InstantiateUtil : MonoBehaviour
+    {
+        public GameObject objectToInstantiate;
+
+        public void DropLoot()
+        {
+            Instantiate(objectToInstantiate, transform.position, Quaternion.identity);
+        }
+
+    }
+}
