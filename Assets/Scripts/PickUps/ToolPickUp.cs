8d4bc078b79a13a2f7bf80ff65d87f1ac58a3345
Joymg <joy.marquezg@gmail.com>
2025-04-11 18:55:03 +0200

    Create Weapon / Tool PickUp and prefabs

---
+++ Assets/Scripts/PickUps/ToolPickUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Joymg.Platformer2D.WeaponSystem;
+using UnityEngine;
+
+namespace Joymg.Platformer2D.Entities.PickUps
+{
+    public class ToolPickUp : PickUp
+    {
+
+        [SerializeField] private WeaponData weaponData;
+
+        private void Start()
+        {
+            _spriteRenderer.sprite = weaponData.sprite;
+        }
+
+        public override void Collect(Agent agent)
+        {
+            agent.PickUp(weaponData);
+        }
+    }
+}
