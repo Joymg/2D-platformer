ba44f82bd27a7dedf9c6131738f28e9b4665efde
Joymg <joy.marquezg@gmail.com>
2025-04-12 13:00:59 +0200

    Create ScoreSystem and Score PickUp scripts

---
+++ Assets/Scripts/PickUps/ScorePickUp.cs
@@ -0,0 +1,19 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Joymg.Platformer2D.Entities.PickUps
+{
+    public class ScorePickUp : PickUp
+    {
+        public UnityEvent OnPickUp;
+        [SerializeField] public int value = 1;
+        public override void Collect(Agent agent)
+        {
+            ScoreSystem scoreSystem = agent.GetComponent<ScoreSystem>();
+            scoreSystem.Add(value);
+            OnPickUp.Invoke();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ScoreSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Player
+{
+    public class ScoreSystem : MonoBehaviour
+    {
+        public UnityEvent<int> OnPointsValueChange;
+        public UnityEvent OnPickUpPoints;
+        private int _points = 0;
+        
+        public int Points
+        {
+            get => _points;
+            set => _points = value;
+        }
+
+        public void Add(int amount)
+        {
+            Points += amount;
+            OnPickUpPoints?.Invoke();
+            OnPointsValueChange?.Invoke(Points);
+        }
+    }
+}
