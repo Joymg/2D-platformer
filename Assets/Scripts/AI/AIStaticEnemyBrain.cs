988de83c820b57de4d27abcb590e2e7d4667c5d5
Joymg <joy.marquezg@gmail.com>
2025-04-14 16:08:26 +0200

    Create brain for static enemies

---
+++ Assets/Scripts/AI/AIStaticEnemyBrain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Joymg.Platformer2D.AI;
+using Joymg.Platformer2D.AI.Behaviours;
+using Joymg.Platformer2D.Input;
+
+namespace Joymg.Platformer2D.AI
+{
+    public class AIStaticEnemyBrain : AIEnemy
+    {
+        public AIBehaviour attackBehaviour;
+
+        private void Update()
+        {
+            attackBehaviour.PerformAction(this);
+        }
+    }
+}
